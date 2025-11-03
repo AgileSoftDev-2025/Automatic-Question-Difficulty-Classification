2a067a5e1a9db46834e3369dd2f8ef8cf4afaeae
Abhinaya Zaim <abhinayazaim.az13@gmail.com>
2025-11-03 22:26:29 +0700

    menambahkan page klasifikasi

--- apps/klasifikasi/urls.py
+++ apps/klasifikasi/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # History URLs
     path('history/', views.history_view, name='history'),
     path('classification/<int:pk>/delete/', views.delete_classification, name='delete_classification'),
-    path('classification/<int:pk>/detail/', views.view_classification_detail, name='classification_detail'),
+    path('<int:pk>/detail/', views.view_classification_detail, name='classification_detail'),
     path('classification/<int:pk>/download/', views.download_report, name='download_report'),
     path('classification/<int:pk>/report/', views.view_report, name='view_report'),
 ]
